a1c85527d0a117c79f9bdadc729a1953ab42cf78
lostjarred <lostotaku@outlook.com>
2021-02-12 17:35:39 +1100

    added BiomeRegistry

---
+++ src/main/java/com/lo93/locore/init/registry/BiomeRegistry.java
@@ -0,0 +1,19 @@
+package com.lo93.locore.init.registry;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.registries.IForgeRegistry;
+
+public class BiomeRegistry {
+	private Logger logger;
+	
+	public BiomeRegistry(Logger loggerin) {
+		this.logger = loggerin;
+	}
+	
+ 	public void RegisterBiome(IForgeRegistry<Biome> registry, Biome biomein) {
+ 		registry.register(biomein);
+ 		logger.info("Registered " + biomein.getRegistryName().toString());
+ 	}
+}
