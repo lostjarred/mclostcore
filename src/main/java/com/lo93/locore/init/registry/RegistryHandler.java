8e87d9ba5591586c2fa805437491bc58b1376e63
lostjarred <lostotaku@outlook.com>
2021-02-16 19:11:58 +1100

    added test item

--- src/main/java/com/lo93/locore/init/registry/RegistryHandler.java
+++ src/main/java/com/lo93/locore/init/registry/RegistryHandler.java
@@ -1,6 +1,12 @@
 package com.lo93.locore.init.registry;
 
+import com.lo93.locore.LoCoreMain;
+import com.lo93.locore.init.items.ModItems;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,6 +19,19 @@ public class RegistryHandler {
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event){
 		IForgeRegistry<Item> registry = event.getRegistry();
+		//test item
+		//LoCoreMain.ITEMREGISTRY.RegisterItem(registry, ModItems.TESTITEM);
+	}
+	
+	@SubscribeEvent
+	public static void registerBiomes(RegistryEvent.Register<Biome> event){
+		IForgeRegistry<Biome> registry = event.getRegistry();
+	}
+	
+	@SubscribeEvent
+	public static void registerModels(ModelRegistryEvent event){
+		//test item model
+		//ModItems.TESTITEM.RegisterItemModel(new ModelResourceLocation("minecraft:apple" ,"inventory"));	
 	}
 	
 	
