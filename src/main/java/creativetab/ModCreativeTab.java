e5f18935e4b59b704c4a8660fe964bc12af6db67
lostjarred <lostotaku@outlook.com>
2021-03-19 13:46:29 +1100

    add creative tab, gradlew

---
+++ src/main/java/creativetab/ModCreativeTab.java
@@ -0,0 +1,23 @@
+package creativetab;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ModCreativeTab extends CreativeTabs {
+	Item creativetabicon;
+	
+	public ModCreativeTab(String creativetabname, Item creativetabiconin) {
+		super(creativetabname+".creativetab");
+		this.creativetabicon = creativetabiconin;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public ItemStack createIcon() {
+		return new ItemStack(creativetabicon);
+	}
+
+}
