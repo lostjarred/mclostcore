8f9275a2faa2e5649abc820c7613d270cc2b57ca
jarred <lostone1993@hotmail.com>
2021-03-27 13:21:44 +1100

    Updated Resource Helper, made public

--- src/main/java/com/lo93/locore/utils/resourcelocation/HelperResourceLocation.java
+++ src/main/java/com/lo93/locore/utils/resourcelocation/HelperResourceLocation.java
@@ -5,12 +5,12 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 public class HelperResourceLocation {
 	private String modid;
 	
-	HelperResourceLocation(String modid){
+	public HelperResourceLocation(String modid){
 		this.modid = modid;
 	}
 	
 	public ModelResourceLocation genModelResourcelocation(String modelname) {
-		ModelResourceLocation modellocation = new ModelResourceLocation(modid + ":" + modelname);
+		ModelResourceLocation modellocation = new ModelResourceLocation(modid + ":" + modelname, "inventory");
 		return modellocation;
 	}
 }
